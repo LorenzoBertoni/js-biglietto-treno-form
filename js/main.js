1a3866ab29c07ad9f7ad45262cb49506eaf3eb47
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-05 16:52:14 +0200

    add: elementi grafici per il biglietto + js per gli output style: modificato lo stile del biglietto.

--- js/main.js
+++ js/main.js
@@ -12,19 +12,31 @@ let over65Discount = ((ticketPrice * 40) / 100);
 let minorPrice = ticketPrice - minorDiscount;
 let over65Price = ticketPrice - over65Discount;
 
+let carriageNumber = Math.floor(Math.random() * 11);
+let cpNumber = Math.floor(Math.random() * 10000) + 90000;
+
+
 
 //funzione per il print dei valori
 let submit = document.getElementById('button');
         submit.addEventListener('click',
             function() {
                 document.getElementById('showName').innerHTML = userName;
-                document.getElementById('showKm').innerHTML = kilometers;
-                document.getElementById('showAge').innerHTML = ageRange;
-                document.getElementById('showPrice').innerHTML = ('il prezzo del tuo biglietto è di ' + ticketPrice + ' &euro;');
+
+                document.getElementById('showOffer').innerHTML = 'Biglietto Standard';
+                    if(ageRange == 'minorenne' || ageRange == 'over65'){
+                        document.getElementById('showOffer').innerHTML = 'Biglietto Ridotto';
+                    }
+
+                document.getElementById('showCarriage').innerHTML = carriageNumber;   
+
+                document.getElementById('showCode').innerHTML = cpNumber;
+
+                document.getElementById('showPrice').innerHTML = (ticketPrice + ' &euro;');
                     if (ageRange == 'minorenne') {
-                        document.getElementById('showPrice').innerHTML = ('il prezzo del tuo biglietto è di ' + minorPrice + ' &euro;');
+                        document.getElementById('showPrice').innerHTML = (minorPrice + ' &euro;');
                     } else if (ageRange == 'over65') {
-                        document.getElementById('showPrice').innerHTML = ('il prezzo del tuo biglietto è di ' + over65Price + ' &euro;');
+                        document.getElementById('showPrice').innerHTML = (over65Price + ' &euro;');
                     } else {
                         alert('seleziona la tua età');
                     }
