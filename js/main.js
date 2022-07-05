d3ad7a836cc58a75cb3cfd240d33edc93ac4f293
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-05 17:51:46 +0200

    fix: js ora prende gli input all'evento e non più al caricamento della pagina. feature: il bottone "ANNULLA" ora refresha la pagina

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
-const userName = document.getElementById('name').value;
-
-let kilometers = document.getElementById('kilometers').value;
-
-let ticketPrice = kilometers * 0.21;
-
-let ageRange = document.getElementById('age').value;
-
-//variabili sconto
-let minorDiscount = ((ticketPrice * 20) / 100);
-let over65Discount = ((ticketPrice * 40) / 100);
-
-//variabili prezzo scontato
-let minorPrice = ticketPrice - minorDiscount;
-let over65Price = ticketPrice - over65Discount;
-
-//variabli per numeri random
-let carriageNumber = Math.floor(Math.random() * 10) + 1;
-let cpNumber = Math.floor(Math.random() * 10000) + 90000;
-
-let showTicket = document.querySelector('.ticket');
 
 //funzione per il print dei valori
 let submit = document.getElementById('button');
         submit.addEventListener('click',
             function() {
+                const userName = document.getElementById('name').value;
+
+                let kilometers = document.getElementById('kilometers').value;
+
+                let ticketPrice = kilometers * 0.21;
+
+                let ageRange = document.getElementById('age').value;
+
+                //variabili sconto
+                let minorDiscount = ((ticketPrice * 20) / 100);
+                let over65Discount = ((ticketPrice * 40) / 100);
+
+                //variabili prezzo scontato
+                let minorPrice = ticketPrice - minorDiscount;
+                let over65Price = ticketPrice - over65Discount;
+
+                //variabli per numeri random
+                let carriageNumber = Math.floor(Math.random() * 10) + 1;
+                let cpNumber = Math.floor(Math.random() * 10000) + 90000;
+
+                let showTicket = document.querySelector('.ticket');
+
                 showTicket.classList.add('d-block');
 
                 document.getElementById('showName').innerHTML = userName;
@@ -42,8 +43,6 @@ let submit = document.getElementById('button');
                         document.getElementById('showPrice').innerHTML = (minorPrice.toFixed(2) + ' &euro;');
                     } else if (ageRange == 'over65') {
                         document.getElementById('showPrice').innerHTML = (over65Price.toFixed(2) + ' &euro;');
-                    } else {
-                        alert('seleziona la tua età');
                     }
             })
 
